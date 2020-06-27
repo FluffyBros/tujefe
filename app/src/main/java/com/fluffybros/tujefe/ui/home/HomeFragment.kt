f5422172dd5755f904b77d68bbe787e7f62bf95b
= <sramesh5@ucsc.edu>
2020-06-27 16:52:15 -0700

    Populate CardView

--- app/src/main/java/com/fluffybros/tujefe/ui/home/HomeFragment.kt
+++ app/src/main/java/com/fluffybros/tujefe/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
 import com.fluffybros.tujefe.R
 
 class HomeFragment : Fragment() {
@@ -22,13 +23,13 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.recycler_home)
-        homeViewModel.text.observe(
+        val recyclerView: RecyclerView = root.findViewById(R.id.recycler_home)
+        /*homeViewModel.text.observe(
             viewLifecycleOwner,
             Observer {
-                textView.text = it
+                recyclerView.text = it
             }
-        )
+        )*/
         return root
     }
 }
