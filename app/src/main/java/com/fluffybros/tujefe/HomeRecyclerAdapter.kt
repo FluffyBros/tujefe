621f304bd4264393ab9684ea34045c39bc5fe5af
= <sramesh5@ucsc.edu>
2020-06-28 13:45:26 -0700

    Fixes Ktlint and removes comments

--- app/src/main/java/com/fluffybros/tujefe/HomeRecyclerAdapter.kt
+++ app/src/main/java/com/fluffybros/tujefe/HomeRecyclerAdapter.kt
@@ -3,22 +3,21 @@ package com.fluffybros.tujefe
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.home_recycler_item.view.*
 
-class HomeRecyclerAdapter(private val itemList: List<HomeRecyclerItem>): RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>() {
-    class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+class HomeRecyclerAdapter(private val itemList: List<HomeRecyclerItem>) : RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>() {
+    class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val accNameView: TextView = itemView.acc_name
         val accCodeView: TextView = itemView.acc_code
-        //val copyCodeButton: Button = itemView.copy_code_button
-        //val editAccButton: Button = itemView.edit_acc_button
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.home_recycler_item,
-            parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.home_recycler_item,
+            parent, false
+        )
 
         return HomeViewHolder(itemView)
     }
@@ -28,11 +27,9 @@ class HomeRecyclerAdapter(private val itemList: List<HomeRecyclerItem>): Recycle
 
         holder.accCodeView.text = currentItem.acc_code
         holder.accNameView.text = currentItem.acc_name
-        //holder.copyCodeButton.
     }
 
     override fun getItemCount(): Int {
         return itemList.size
     }
-
 }
--- app/src/main/java/com/fluffybros/tujefe/ui/home/HomeFragment.kt
+++ app/src/main/java/com/fluffybros/tujefe/ui/home/HomeFragment.kt
@@ -4,15 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.fluffybros.tujefe.HomeRecyclerAdapter
 import com.fluffybros.tujefe.HomeRecyclerItem
-import com.fluffybros.tujefe.MainActivity
 import com.fluffybros.tujefe.R
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -25,16 +20,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //homeViewModel =
-        //    ViewModelProviders.of(this).get(HomeViewModel::class.java)
-
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        /*homeViewModel.text.observe(
-            viewLifecycleOwner,
-            Observer {
-                recyclerView.text = it
-            }
-        )*/
         return root
     }
 
@@ -47,9 +33,9 @@ class HomeFragment : Fragment() {
         }
     }
 
-    private fun generateHomeRecyclerItems(): List<HomeRecyclerItem>{
+    private fun generateHomeRecyclerItems(): List<HomeRecyclerItem> {
         val list = ArrayList<HomeRecyclerItem>()
-        for(i in 0 until 100){
+        for (i in 0 until 100) {
             val item = HomeRecyclerItem(i, "Account $i", "000-000")
             list += item
         }
