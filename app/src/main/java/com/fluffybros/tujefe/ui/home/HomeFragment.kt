46635da1daa75bd00bcd0536919645aa42e3ca36
= <sramesh5@ucsc.edu>
2020-06-27 19:28:15 -0700

    RecyclerView is fully working

--- app/src/main/java/com/fluffybros/tujefe/ui/home/HomeFragment.kt
+++ app/src/main/java/com/fluffybros/tujefe/ui/home/HomeFragment.kt
@@ -8,8 +8,13 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.fluffybros.tujefe.HomeRecyclerAdapter
+import com.fluffybros.tujefe.HomeRecyclerItem
+import com.fluffybros.tujefe.MainActivity
 import com.fluffybros.tujefe.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -20,10 +25,10 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        //homeViewModel =
+        //    ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val recyclerView: RecyclerView = root.findViewById(R.id.recycler_home)
         /*homeViewModel.text.observe(
             viewLifecycleOwner,
             Observer {
@@ -31,5 +36,23 @@ class HomeFragment : Fragment() {
             }
         )*/
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val exampleList = generateHomeRecyclerItems()
+        recycler_home.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = HomeRecyclerAdapter(exampleList)
+        }
+    }
+
+    private fun generateHomeRecyclerItems(): List<HomeRecyclerItem>{
+        val list = ArrayList<HomeRecyclerItem>()
+        for(i in 0 until 100){
+            val item = HomeRecyclerItem(i, "Account $i", "000-000")
+            list += item
+        }
+        return list
     }
 }
