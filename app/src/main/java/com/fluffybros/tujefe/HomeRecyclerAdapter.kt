78f85e25bdd9d2e8c1d46873b82128ab76666c6d
= <sramesh5@ucsc.edu>
2020-06-27 18:15:44 -0700

    Implements override functions in the Adapter

--- app/src/main/java/com/fluffybros/tujefe/HomeRecyclerAdapter.kt
+++ app/src/main/java/com/fluffybros/tujefe/HomeRecyclerAdapter.kt
@@ -1,5 +1,6 @@
 package com.fluffybros.tujefe
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -7,24 +8,31 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.home_recycler_item.view.*
 
-class HomeRecyclerAdapter: RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>() {
+class HomeRecyclerAdapter(private val itemList: List<HomeRecyclerItem>): RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>() {
     class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val accNameView: TextView = itemView.acc_name
         val accCodeView: TextView = itemView.acc_code
-        val copyCodeButton: Button = itemView.copy_code_button
-        val editAccButton: Button = itemView.edit_acc_button
+        //val copyCodeButton: Button = itemView.copy_code_button
+        //val editAccButton: Button = itemView.edit_acc_button
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        TODO("Not yet implemented")
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.home_recycler_item,
+            parent, false)
+
+        return HomeViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentItem = itemList[position]
+
+        holder.accCodeView.text = currentItem.acc_code
+        holder.accNameView.text = currentItem.acc_name
+        //holder.copyCodeButton.
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return itemList.size
     }
 
 }
