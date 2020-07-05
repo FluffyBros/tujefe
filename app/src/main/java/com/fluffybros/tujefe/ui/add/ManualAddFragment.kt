d2a2bc99b0be20e438c540123deaab377dd4417b
= <sramesh5@ucsc.edu>
2020-07-04 21:06:59 -0700

    Fix lint errors

--- app/src/main/java/com/fluffybros/tujefe/ui/add/ManualAddFragment.kt
+++ app/src/main/java/com/fluffybros/tujefe/ui/add/ManualAddFragment.kt
@@ -5,10 +5,10 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.fluffybros.tujefe.R
 
-class ManualAddFragment: Fragment(R.layout.fragment_manual_add) {
+class ManualAddFragment : Fragment(R.layout.fragment_manual_add) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //val binding = FragmentManualAddBinding.bind(view)
+        // val binding = FragmentManualAddBinding.bind(view)
     }
 }
