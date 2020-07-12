38a273841f648a4554d4bb5367f9ec56f9bd04dd
= <sramesh5@ucsc.edu>
2020-07-12 12:39:48 -0700

    Add onClickListener to Add button in ManualAddFragment

--- app/src/main/java/com/fluffybros/tujefe/ui/add/ManualAddFragment.kt
+++ app/src/main/java/com/fluffybros/tujefe/ui/add/ManualAddFragment.kt
@@ -1,6 +1,24 @@
 package com.fluffybros.tujefe.ui.add
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.fluffybros.tujefe.MainViewModel
 import com.fluffybros.tujefe.R
+import com.fluffybros.tujefe.databinding.FragmentManualAddBinding
 
-class ManualAddFragment : Fragment(R.layout.fragment_manual_add)
+class ManualAddFragment : Fragment(R.layout.fragment_manual_add) {
+    private val mainViewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentManualAddBinding.bind(view)
+
+        binding.manualAddButton.setOnClickListener {
+            mainViewModel.addRecyclerItem("NEW ACCOUNT", "444444")
+        }
+    }
+}
