ac78e1659d9e0b7e946b7e361102c45468f178ac
Sriram Ramesh <sramesh5@ucsc.edu>
2020-07-14 12:42:07 -0700

    ManualAddFragment properly adds items to the RecyclerView
    ManualAddFragment properly adds items to the RecyclerView

---
+++ app/src/main/java/com/fluffybros/tujefe/MainViewModel.kt
@@ -0,0 +1,17 @@
+package com.fluffybros.tujefe
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.fluffybros.tujefe.ui.home.HomeRecyclerItem
+
+class MainViewModel : ViewModel() {
+    private val _homeList = MutableLiveData<List<HomeRecyclerItem>>(listOf())
+
+    val homeList: LiveData<List<HomeRecyclerItem>> = _homeList
+
+    fun addRecyclerItem(name: String, code: String) {
+        val newItem = HomeRecyclerItem(_homeList.value?.size ?: 0, name, code)
+        _homeList.value = _homeList.value?.plus(newItem)
+    }
+}
--- app/src/main/java/com/fluffybros/tujefe/ui/add/ManualAddFragment.kt
+++ app/src/main/java/com/fluffybros/tujefe/ui/add/ManualAddFragment.kt
@@ -1,6 +1,28 @@
 package com.fluffybros.tujefe.ui.add
 
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.fluffybros.tujefe.MainViewModel
 import com.fluffybros.tujefe.R
+import com.fluffybros.tujefe.databinding.FragmentManualAddBinding
 
-class ManualAddFragment : Fragment(R.layout.fragment_manual_add)
+class ManualAddFragment : Fragment(R.layout.fragment_manual_add) {
+    private val mainViewModel: MainViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentManualAddBinding.bind(view)
+
+        binding.manualAddButton.setOnClickListener {
+            mainViewModel.addRecyclerItem(binding.nameField.editText?.text.toString(), binding.setupKeyField.editText?.text.toString())
+            val imm = requireContext().getSystemService<InputMethodManager>()
+            imm?.hideSoftInputFromWindow(view.windowToken, 0)
+            findNavController().navigate(R.id.navigation_home)
+        }
+    }
+}
