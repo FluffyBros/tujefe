785c84561e3545b079353b70fb73c7ac597b924d
= <sramesh5@ucsc.edu>
2020-07-13 22:45:18 -0700

    Change ArrayList() in MainViewModel to listOf()

--- app/src/main/java/com/fluffybros/tujefe/MainViewModel.kt
+++ app/src/main/java/com/fluffybros/tujefe/MainViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel
 import com.fluffybros.tujefe.ui.home.HomeRecyclerItem
 
 class MainViewModel : ViewModel() {
-    private val _homeList = MutableLiveData<List<HomeRecyclerItem>>(ArrayList())
+    private val _homeList = MutableLiveData<List<HomeRecyclerItem>>(listOf())
 
     val homeList: LiveData<List<HomeRecyclerItem>> = _homeList
 
