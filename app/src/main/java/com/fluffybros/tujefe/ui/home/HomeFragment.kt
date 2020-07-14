7db68a4cdf295e88d9e24d14148be45e59f4fba6
= <sramesh5@ucsc.edu>
2020-07-13 17:53:16 -0700

    Clean code in HomeFragment

--- app/src/main/java/com/fluffybros/tujefe/ui/home/HomeFragment.kt
+++ app/src/main/java/com/fluffybros/tujefe/ui/home/HomeFragment.kt
@@ -18,9 +18,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentHomeBinding.bind(view)
-        binding.recyclerHome.apply {
-            layoutManager = LinearLayoutManager(activity)
-        }
+        binding.recyclerHome.layoutManager = LinearLayoutManager(requireContext())
 
         mainViewModel.homeList.observe(
             viewLifecycleOwner,
