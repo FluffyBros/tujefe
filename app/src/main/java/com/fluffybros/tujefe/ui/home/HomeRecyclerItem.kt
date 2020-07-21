fdd3de467f9be8e0b3ed437e8a01a928f28f2e65
= <sramesh5@ucsc.edu>
2020-07-21 13:01:38 -0700

    Fix lint errors

--- app/src/main/java/com/fluffybros/tujefe/ui/home/HomeRecyclerItem.kt
+++ app/src/main/java/com/fluffybros/tujefe/ui/home/HomeRecyclerItem.kt
@@ -3,5 +3,5 @@ package com.fluffybros.tujefe.ui.home
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName= "home_recycler_item")
+@Entity(tableName = "home_recycler_item")
 data class HomeRecyclerItem(@PrimaryKey val id: Int, var name: String, var code: String)
