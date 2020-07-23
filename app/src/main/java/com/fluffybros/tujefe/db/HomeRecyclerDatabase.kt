7eb0aaaa5132b422d74c7075567ef2526911f025
= <sramesh5@ucsc.edu>
2020-07-22 17:45:40 -0700

    Fix lint errors

--- app/src/main/java/com/fluffybros/tujefe/db/HomeRecyclerDatabase.kt
+++ app/src/main/java/com/fluffybros/tujefe/db/HomeRecyclerDatabase.kt
@@ -5,7 +5,6 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-
 @Database(entities = arrayOf(HomeRecyclerItem::class), version = 1, exportSchema = false)
 public abstract class HomeRecyclerDatabase : RoomDatabase() {
 
