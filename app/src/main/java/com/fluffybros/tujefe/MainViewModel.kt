5c6cfcb8fbf6a9fed79cb0864e72bf6e3c6e1f79
= <sramesh5@ucsc.edu>
2020-07-23 10:38:27 -0700

    Change MainViewModel's LiveData to initialize using the database

--- app/src/main/java/com/fluffybros/tujefe/MainViewModel.kt
+++ app/src/main/java/com/fluffybros/tujefe/MainViewModel.kt
@@ -1,14 +1,23 @@
 package com.fluffybros.tujefe
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.fluffybros.tujefe.db.HomeRecyclerDatabase
 import com.fluffybros.tujefe.db.HomeRecyclerItem
+import com.fluffybros.tujefe.db.HomeRepository
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val _homeList = MutableLiveData<List<HomeRecyclerItem>>(listOf())
+    private val repository : HomeRepository
+    val homeList: LiveData<List<HomeRecyclerItem>>
 
-    val homeList: LiveData<List<HomeRecyclerItem>> = _homeList
+    init {
+        val homeRecyclerDao = HomeRecyclerDatabase.getDatabase(application).homeRecyclerDao()
+        repository = HomeRepository(homeRecyclerDao)
+        homeList = repository.allHomeRecyclerItems
+    }
 
     fun addRecyclerItem(name: String, code: String) {
         val newItem = HomeRecyclerItem(
