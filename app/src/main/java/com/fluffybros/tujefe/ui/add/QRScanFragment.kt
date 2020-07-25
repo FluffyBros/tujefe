ac46c6b72d35340953781b0cb38629712c220c9e
= <sramesh5@ucsc.edu>
2020-07-25 14:32:45 -0700

    Make QRScanFragment start scanning for QR codes when created

--- app/src/main/java/com/fluffybros/tujefe/ui/add/QRScanFragment.kt
+++ app/src/main/java/com/fluffybros/tujefe/ui/add/QRScanFragment.kt
@@ -2,18 +2,45 @@ package com.fluffybros.tujefe.ui.add
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
 import com.fluffybros.tujefe.MainViewModel
 import com.fluffybros.tujefe.R
 import com.fluffybros.tujefe.databinding.FragmentQrScanBinding
 
 class QRScanFragment : Fragment(R.layout.fragment_qr_scan) {
     private val mainViewModel: MainViewModel by activityViewModels()
+    private lateinit var codeScanner: CodeScanner
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentQrScanBinding.bind(view)
-        
+        val scannerView = binding.scannerView
+
+        codeScanner = CodeScanner(requireActivity(), scannerView)
+
+        codeScanner = CodeScanner(requireActivity(), scannerView)
+        codeScanner.decodeCallback = DecodeCallback {
+            activity?.runOnUiThread {
+                Toast.makeText(activity, it.text, Toast.LENGTH_LONG).show()
+            }
+        }
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
 }
