e0c152b4e1c1a17f77c338d44089d3e7c848ce28
Sriram Ramesh <sramesh5@ucsc.edu>
2020-07-24 19:13:22 -0700

    Create repository file for HomeFragment's RecyclerView (#16)
    Create repository file for HomeFragment's RecyclerView

---
+++ app/src/main/java/com/fluffybros/tujefe/db/HomeRepository.kt
@@ -0,0 +1,12 @@
+package com.fluffybros.tujefe.db
+
+import androidx.lifecycle.LiveData
+
+class HomeRepository(private val homeRecyclerDao: HomeRecyclerDao) {
+
+    val allHomeRecyclerItems: LiveData<List<HomeRecyclerItem>> = homeRecyclerDao.getAlphabetizedWords()
+
+    suspend fun insert(homeRecyclerItem: HomeRecyclerItem) {
+        homeRecyclerDao.insert(homeRecyclerItem)
+    }
+}
