1f5f05766d418cc39a83d8fa6a126699a54b691d
= <sramesh5@ucsc.edu>
2020-07-28 15:34:29 -0700

    Add supporting classes for barcode scanning from google vision sample

---
+++ app/src/main/java/com/fluffybros/tujefe/ui/add/camera/BarcodeTrackerFactory.kt
@@ -0,0 +1,21 @@
+package com.fluffybros.tujefe.ui.add.camera
+
+import android.content.Context
+import com.google.android.gms.vision.MultiProcessor
+import com.google.android.gms.vision.barcode.Barcode
+
+
+/**
+ * Factory for creating a tracker and associated graphic to be associated with a new barcode.  The
+ * multi-processor uses this factory to create barcode trackers as needed -- one for each barcode.
+ */
+internal class BarcodeTrackerFactory(
+    private val mGraphicOverlay: GraphicOverlay<BarcodeGraphic>,
+    private val mContext: Context
+) : MultiProcessor.Factory<Barcode> {
+    override fun create(barcode: Barcode): BarcodeGraphicTracker {
+        val graphic = BarcodeGraphic(mGraphicOverlay)
+        return BarcodeGraphicTracker(mGraphicOverlay, graphic, mContext)
+    }
+
+}
