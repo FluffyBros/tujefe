eda303dabf3ad8f8e267836db2a4374bc1bc6bee
= <sramesh5@ucsc.edu>
2020-07-30 12:32:27 -0700

    Parse barcode information to create RecyclerItems with valid fields

--- app/src/main/java/com/fluffybros/tujefe/MainActivity.kt
+++ app/src/main/java/com/fluffybros/tujefe/MainActivity.kt
@@ -39,7 +39,26 @@ class MainActivity : AppCompatActivity(), BarcodeGraphicTracker.BarcodeUpdateLis
             Log.d("Barcode-Reader", barcode.displayValue)
             Log.d("Barcode-Reader", barcode.format.toString())
 
-            mainViewModel.addRecyclerItem(barcode.displayValue, barcode.rawValue)
+            val secretSearch = ArrayList<String>()
+            secretSearch.add("secret=")
+            val codeStart = barcode.displayValue.findAnyOf(secretSearch, 0, false)
+            val secretEndSearch = ArrayList<String>()
+            secretEndSearch.add("&")
+            val codeEnd = barcode.displayValue.findAnyOf(secretEndSearch, codeStart?.first ?: 0, false )
+            val codeIndices = IntRange(codeStart?.first?.plus(7)?:0, codeEnd?.first?.minus(1)?:barcode.displayValue.lastIndex)
+            val code = barcode.displayValue.substring(codeIndices )
+
+            val issuerSearch = ArrayList<String>()
+            issuerSearch.add("issuer=")
+            val nameStart = barcode.displayValue.findAnyOf(issuerSearch, 0, false)
+            val issuerEndSearch = ArrayList<String>()
+            issuerEndSearch.add("&")
+            val nameEnd = barcode.displayValue.findAnyOf(issuerEndSearch, nameStart?.first ?: 0, false )
+            val nameIndices = IntRange(nameStart?.first?.plus(7)?:0, nameEnd?.first?.minus(1)?:barcode.displayValue.lastIndex)
+            val name = barcode.displayValue.substring(nameIndices )
+
+
+            mainViewModel.addRecyclerItem(name, code)
             runOnUiThread { navController.navigate(R.id.navigation_home) }
         }
     }
