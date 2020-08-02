8a05194433da0e6a2b0b5ecf951ea21df739f9fa
= <sramesh5@ucsc.edu>
2020-08-02 14:22:45 -0700

    Make cameraButton launch an Intent to open the camera

--- app/src/main/java/com/fluffybros/tujefe/ui/add/QRScanFragment.kt
+++ app/src/main/java/com/fluffybros/tujefe/ui/add/QRScanFragment.kt
@@ -1,19 +1,30 @@
 package com.fluffybros.tujefe.ui.add
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.fluffybros.tujefe.R
 import com.fluffybros.tujefe.databinding.FragmentQrScanBinding
 
 class QRScanFragment : Fragment(R.layout.fragment_qr_scan) {
+    private val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentQrScanBinding.bind(view)
 
         binding.cameraButton.setOnClickListener {
-            
+            dispatchTakePictureIntent()
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
         }
     }
 }
