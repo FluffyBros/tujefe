1f03847ef00e1854a86abd66d610cb015d5bf1d6
= <sramesh5@ucsc.edu>
2020-08-09 15:36:34 -0700

    Create initial onClickListener for EditButton in HomeRecyclerItem

--- app/src/main/java/com/fluffybros/tujefe/ui/home/HomeRecyclerAdapter.kt
+++ app/src/main/java/com/fluffybros/tujefe/ui/home/HomeRecyclerAdapter.kt
@@ -1,12 +1,19 @@
 package com.fluffybros.tujefe.ui.home
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.fluffybros.tujefe.MainActivity
+import com.fluffybros.tujefe.R
 import com.fluffybros.tujefe.databinding.HomeRecyclerItemBinding
 import com.fluffybros.tujefe.db.HomeRecyclerItem
+import com.fluffybros.tujefe.ui.edit.EditFragment
+import kotlinx.android.synthetic.main.fragment_container.view.*
 
-class HomeRecyclerAdapter(private val itemList: List<HomeRecyclerItem>) : RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>() {
+class HomeRecyclerAdapter(private val itemList: List<HomeRecyclerItem>, private val activity: FragmentActivity) : RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>() {
 
     class HomeViewHolder(val binding: HomeRecyclerItemBinding) : RecyclerView.ViewHolder(binding.root)
 
@@ -19,6 +26,12 @@ class HomeRecyclerAdapter(private val itemList: List<HomeRecyclerItem>) : Recycl
         val currentItem = itemList[position]
         holder.binding.code.text = currentItem.code
         holder.binding.name.text = currentItem.name
+        holder.binding.editButton.setOnClickListener {
+            val transaction = activity.supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentContainer, EditFragment(position))
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
     }
 
     override fun getItemCount(): Int {
