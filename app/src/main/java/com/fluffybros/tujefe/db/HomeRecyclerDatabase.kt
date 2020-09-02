9551b49465f6ebee3598923c3fa668c4ef70529f
= <sramesh5@ucsc.edu>
2020-09-02 08:27:34 -0700

    Add converter to Room Database

--- app/src/main/java/com/fluffybros/tujefe/db/HomeRecyclerDatabase.kt
+++ app/src/main/java/com/fluffybros/tujefe/db/HomeRecyclerDatabase.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
-@Database(entities = arrayOf(HomeRecyclerItem::class), version = 1, exportSchema = false)
-public abstract class HomeRecyclerDatabase : RoomDatabase() {
+@Database(entities = [HomeRecyclerItem::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
+abstract class HomeRecyclerDatabase : RoomDatabase() {
 
     abstract fun homeRecyclerDao(): HomeRecyclerDao
 
