b79ed3e3b28ff7e05d6dd2f72f510a9dbc21653d
= <sramesh5@ucsc.edu>
2020-09-16 13:01:00 -0700

    Merge master

--- app/src/main/java/com/fluffybros/tujefe/ui/home/HomeRecyclerAdapter.kt
+++ app/src/main/java/com/fluffybros/tujefe/ui/home/HomeRecyclerAdapter.kt
@@ -1,18 +1,31 @@
 package com.fluffybros.tujefe.ui.home
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Context.CLIPBOARD_SERVICE
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.fluffybros.tujefe.databinding.HomeRecyclerItemBinding
 import com.fluffybros.tujefe.db.HomeRecyclerItem
 
-class HomeRecyclerAdapter(private val itemList: List<HomeRecyclerItem>) : RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>() {
+
+class HomeRecyclerAdapter(
+    private val itemList: List<HomeRecyclerItem>,
+    private val context: Context
+) : RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>() {
 
     class HomeViewHolder(val binding: HomeRecyclerItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        val binding = HomeRecyclerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = HomeRecyclerItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
         return HomeViewHolder(binding)
     }
 
@@ -23,6 +36,12 @@ class HomeRecyclerAdapter(private val itemList: List<HomeRecyclerItem>) : Recycl
         holder.binding.editButton.setOnClickListener {
             val action = HomeFragmentDirections.actionNavigationHomeToNavigationEdit(position)
             holder.itemView.findNavController().navigate(action)
+        }
+        holder.binding.copyCodeButton.setOnClickListener {
+            val clipboard = getSystemService(context, Class<ClipboardManager>()) as ClipboardManager?
+            val clip = ClipData.newPlainText("label", "Text to copy")
+            clipboard!!.setPrimaryClip(clip)
+
         }
     }
 
